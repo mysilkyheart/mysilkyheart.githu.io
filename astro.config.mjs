6226623e08f576dc01652000fa0346e01ed71cf8
Devi Nufriana <devi.nufriana@gdn-commerce.com>
2023-09-07 16:44:41 +0700

    setup deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://deanufriana.github.io',
+  base: '/vitejs',
   integrations: [mdx(), sitemap(), vue(),
   partytown({
     // Adds dataLayer.push as a forwarding-event.
